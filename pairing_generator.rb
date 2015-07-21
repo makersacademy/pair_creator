a457b6b70f374a2ea390d57e90f7bcd7ede39fd7
Stephen Lloyd <stephen.lloyd85@gmail.com>
2015-07-21 09:48:57 +0100

    refactor of generating file

--- pairing_generator.rb
+++ pairing_generator.rb
@@ -1,16 +1,39 @@
 require 'yaml'
 
-cohort = "june"
+class PairCreator
 
-all_students = YAML.load_file("./cohorts/#{cohort}.yml")
-historic_pairs = Dir["./pairs/#{cohort}-*.yml"].map {|file| YAML.load_file(file) }.inject([]){|m,a| m + a} rescue []
+  VALID_COHORTS = ['january', 'february', 'march', 'april', 'may','june','july','august','september', 'october','novermber', 'december']
+  attr_reader :cohort, :all_students, :historic_pairs
 
-all_pairs = all_students.combination(2).map{|pair|pair.sort}.to_a
+  def initialize cohort
+    @cohort = cohort
+    @all_students = YAML.load_file("./cohorts/#{@cohort}.yml")
+    @historic_pairs = Dir["./pairs/#{cohort}-*.yml"].map {|file| YAML.load_file(file) }.inject([]){|m,a| m + a} rescue []
+  end
 
-day_pairs = []
-day_pairs = all_pairs.select do |pair|
-  day_pairs << pair unless (day_pairs.flatten.include?(pair.first)) || (day_pairs.flatten.include?(pair.last)) || historic_pairs.include?(pair)
+  def all_pairs
+    all_students.combination(2).map{|pair|pair.sort}.to_a
+  end
+
+  def save_to_file(day_pairs)
+    File.write("./pairs/#{cohort}-#{Time.now.strftime("%Y-%d-%m")}-#{rand(10 * 10 * 10)}.yml", day_pairs.to_yaml)
+  end
+
+  def call
+    save_to_file(generate_new_pairs)
+  end
+
+  def generate_new_pairs
+    day_pairs = []
+    day_pairs = all_pairs.select do |pair|
+      day_pairs << pair unless (day_pairs.flatten.include?(pair.first)) || (day_pairs.flatten.include?(pair.last)) || historic_pairs.include?(pair)
+    end
+  end
+
+  def self.call cohort
+    raise "You need to supply a cohort" unless VALID_COHORTS.include?(cohort)
+    new(cohort).call
+  end
 end
 
-File.write("./pairs/#{cohort}-#{Time.now.strftime("%Y-%d-%m")}-#{rand(10 * 10 * 10)}.yml", day_pairs.to_yaml)
-
+PairCreator.call('july')
